86e4ad9eff5f97ced88188debaa82e4a3b9d79ae
johnlimjsj <johnlimjsj@gmail.com>
2017-07-14 23:51:59 +0800

    models

--- Website/ezpz/models.py
+++ Website/ezpz/models.py
@@ -19,13 +19,15 @@ class Services(models.Model):
 	likes = models.IntegerField()
 	rating = models.DecimalField(max_digits=2, decimal_places=1)
 
-class Feedback(models.Model):
-	user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
-	feedback = models.CharField(max_length=65536)
-	date = models.TimeField()
-
 
 class Users(models.Model):
 	name = models.CharField(max_length=255)
 	age = models.IntegerField()
 	designation = models.CharField(max_length=255)
+
+class Feedback(models.Model):
+	user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+	feedback = models.CharField(max_length=65536)
+	date_created = models.TimeField() # should have the time stamp when it is created
+	priority = models.FloatField()
+
